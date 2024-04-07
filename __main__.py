cfb3636ec238352b49f78a567bf7d2f483a13fd4
dewble <dewble62@gmail.com>
2024-04-07 16:18:21 +0900

    feat: Create AWS VPC with pulumi

--- __main__.py
+++ __main__.py
@@ -1,10 +1,20 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
-from pulumi_aws import s3
+from vpc import create_vpc
 
-# Create an AWS resource (S3 Bucket)
-bucket = s3.Bucket('my-bucket')
+
+######## Pulumi 구성 시스템을 사용하여 구성 값을 가져옴 ########
+config = pulumi.Config()
+vpc_cidr_block = config.require("vpc_cidr_block")
+# tags
+tags = config.require_object("tags")
+## project tags 가져오기
+project = tags["Project"]
+
+
+######## Create VPC ########
+vpc = create_vpc(f"{project}-vpc", vpc_cidr_block, tags)
 
 # Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+pulumi.export("vpc_id", vpc.id)
